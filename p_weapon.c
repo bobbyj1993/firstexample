4e705cea66ddf84e8f2bf9b74bf82eb341241cc3
bobbyj1993 <bobby.jovanoski@yahoo.com>
2015-02-04 16:34:37 -0500

    "Changed hyperblaster" Credit to: sumfuka

--- p_weapon.c
+++ p_weapon.c
@@ -837,11 +837,11 @@ void Weapon_Blaster_Fire (edict_t *ent)
 		damage = 10;
 	Blaster_Fire (ent, vec3_origin, damage, false, EF_BLASTER);
 
-	VectorSet(tempvec, 0, 8, 0);
+	VectorSet(tempvec, 0, 6, 0);
 	VectorAdd(tempvec, vec3_origin, tempvec);
 	Blaster_Fire (ent, tempvec, damage, false, EF_BLASTER);
 
-	VectorSet(tempvec, 0, -8, 0);
+	VectorSet(tempvec, 0, -6, 0);
 	VectorAdd(tempvec, vec3_origin, tempvec);
 	Blaster_Fire (ent, tempvec, damage, false, EF_BLASTER);
 
